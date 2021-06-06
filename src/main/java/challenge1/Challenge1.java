62a99e533f124812cebc5aefbeace4d9a69988df
agalvezv <43121059+agalvezv@users.noreply.github.com>
2021-06-05 21:18:04 -0400

    Initial commit

---
+++ src/main/java/challenge1/Challenge1.java
@@ -0,0 +1,15 @@
+package challenge1;
+
+import java.util.Scanner;
+
+public class Challenge1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+
+        String name = input.nextLine();
+
+        System.out.println("Hello, " + name + ", nice to meet you.");
+    }
+}
